3ccb52be9a04c3ba0497a8b810194eb6ebf1d8e8
Ross Bulat <ross@parity.io>
2023-12-06 16:28:38 +0700

    add scripts config

---
+++ scripts/config.mjs
@@ -0,0 +1,17 @@
+// Copyright 2023 @polkadot-cloud/library authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+// ----------------------------------------------
+// Files that are required to exist in a package.
+// ----------------------------------------------
+export const PACKAGE_REQUIRED_FILES = [
+  "README.md",
+  "README.npm.md",
+  "package.json",
+  "lib",
+];
+
+// --------------------------------------------------------------------
+// Scripts that are required to exist in a package's package.json file.
+// --------------------------------------------------------------------
+export const PACKAGE_REQUIRED_SCRIPTS = ["build:mock", "build", "clear"];
