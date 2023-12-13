70d8366f35307bf160993c21ac55f652175b7345
Ross Bulat <ross@parity.io>
2023-12-13 13:24:22 +0700

    don't use strict mode

--- sandbox/src/main.tsx
+++ sandbox/src/main.tsx
@@ -1,13 +1,8 @@
 /* @license Copyright 2023 @polkadot-cloud/library authors & contributors
 SPDX-License-Identifier: GPL-3.0-only */
 
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
