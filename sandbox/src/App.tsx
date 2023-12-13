52a5402b8face37319586c8f3118dcfbc2d40505
Ross Bulat <ross@parity.io>
2023-12-13 11:51:59 +0700

    add sandbox boilerplate

---
+++ sandbox/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./svg/icon-filled.svg";
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo" alt="Polkadot Cloud Logo" />
+      </div>
+      <h1>Polkadot Cloud Sandbox</h1>
+      <div className="card">
+        <button onClick={() => setCount((prev) => prev + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default App;
