6012f624da34e032ceb4720f58dc2b8a561f28d4
Ross Bulat <ross@parity.io>
2023-12-19 09:46:16 +0700

    add LedgerAccountsProvider

---
+++ packages/cloud-react/lib/connect/LedgerAccountsProvider/types.ts
@@ -0,0 +1,25 @@
+// Copyright 2023 @polkadot-cloud/library authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { LedgerAccount } from "../types";
+
+export type LedgerAccountsContextInterface = {
+  ledgerAccountExists: (a: string) => boolean;
+  addLedgerAccount: (
+    a: string,
+    i: number,
+    callback?: () => void
+  ) => LedgerAccount | null;
+  removeLedgerAccount: (a: string, callback?: () => void) => void;
+  renameLedgerAccount: (a: string, name: string) => void;
+  getLedgerAccount: (a: string) => LedgerAccount | null;
+  ledgerAccounts: LedgerAccount[];
+};
+
+export interface LedgerAddress {
+  address: string;
+  index: number;
+  name: string;
+  network: string;
+  pubKey: string;
+}
