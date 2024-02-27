5926c107a0031e47bdbc1bce214e55e965ab92ac
Ross Bulat <ross@parity.io>
2024-02-27 15:27:52 +0700

    remove utils

--- packages/cloud-react/lib/utils/index.ts
+++
@@ -1,6 +0,0 @@
-/* @license Copyright 2024 @polkadot-cloud/library authors & contributors
-SPDX-License-Identifier: GPL-3.0-only */
-
-/* Returns ` t` if truthy, or an empty string otherwise. */
-export const valEmpty = (t: boolean | string | undefined, v: string) =>
-  t ? ` ${v}` : "";
